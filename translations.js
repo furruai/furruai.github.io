bdec0359ca4e0c9e642ea7f93e826123cf5cebb5
Anmenig <35890296+anmenig@users.noreply.github.com>
2024-09-18 06:41:30 +0300

    Create translations.js

---
+++ translations.js
@@ -0,0 +1,10 @@
+var data = {
+  "en": {
+    "welcome": "Welcome to our website!",
+    "description": "This is a multilingual website.",
+  },
+  "fr": {
+    "welcome": "Bienvenue sur notre site!",
+    "description": "Ceci est un site multilingue.",
+  }
+}
